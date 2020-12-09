ba6318cc6e23600a1e117ac9ddb49fbe178c6800
Chris Bowles <BowlesCR@users.noreply.github.com>
2020-12-09 11:11:05 -0500

    Day 9 Part 2

--- Day_09/09_Encoding_Error.py
+++ Day_09/09_Encoding_Error.py
@@ -6,12 +6,24 @@ PREAMBLE: int = 25
 
 def main() -> None:
     data: list[int] = [int(line) for line in fileinput.input()]
+    invalid: int = None
     for i, num in enumerate(data):
         if i < PREAMBLE:
             continue
+
         if num not in [sum(foo) for foo in combinations(data[i-PREAMBLE:i], 2)]:
-            print(num)
+            invalid = num
+            print(f"Part 1: {invalid}")
             break
+
+    if not invalid:
+        raise Exception
+
+    for i in range(len(data)):
+        for j in range(i+2, len(data)):
+            if sum(data[i:j]) == invalid:
+                print(f"Part 2: {min(data[i:j]) + max(data[i:j])}")
+                exit()
 
 
 if __name__ == "__main__":

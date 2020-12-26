9e6a381a0f118ada3f4e64b3d87748244f2c47ac
Chris Bowles <BowlesCR@users.noreply.github.com>
2020-12-26 09:34:17 -0500

    Day 25 Part 1

--- Day_25/25.py
+++ Day_25/25.py
@@ -1,5 +1,43 @@
+import fileinput
+
 def main() -> None:
-    pass
+    with fileinput.input() as fi:
+        cardpub = int(fi.readline())
+        doorpub = int(fi.readline())
+
+
+    cardloop: int = 0
+    doorloop: int = 0
+
+    value = 1
+    i = 1
+    while True:
+        value *= 7
+        value %= 20201227
+
+        if value == cardpub:
+            cardloop = i
+            break
+        if value == doorpub:
+            doorloop = i
+            break
+
+        i += 1
+
+    if cardloop:
+        value = 1
+        for _ in range(cardloop):
+            value *= doorpub
+            value %= 20201227
+        print(value)
+    elif doorloop:
+        value = 1
+        for _ in range(doorloop):
+            value *= cardpub
+            value %= 20201227
+        print(value)
+    else:
+        raise Exception("Neither loop value found!")
 
 
 if __name__ == "__main__":
